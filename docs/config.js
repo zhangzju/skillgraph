2f9463b157313f8921f52701a683322138ee98fd
zhangzju <zhangway19921221@163.com>
2017-03-01 02:56:00 +0800

    add landing page

--- docs/config.js
+++ docs/config.js
@@ -1,3 +1,12 @@
 self.$config = {
-  // config...
+  // true 将会默认使用 `landing.html`
+  landing: true,
+  // 当然 markdown 文件也可以
+  landing: 'landing.md',
+  nav: [
+    // 首页
+    {title: 'Home', path: '/home'},
+    // 中文文档
+    {title: '中文文档', path: '/language/chinese'}
+  ]
 }
---
+++ docs/landing.md
@@ -0,0 +1,1 @@
+# Skill Graph is  landing now~~
